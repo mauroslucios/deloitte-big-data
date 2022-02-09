b11ad23481d9087c5861d3649b04920517fb0ed1
mauroslucios <mauroslucios@gmail.com>
2022-02-08 23:23:14 -0300

    Adicionado TipoFuncionario a classe Funcionario

--- src/main/java/com/big/data/desafio/enums/TipoFuncionario.java
+++ src/main/java/com/big/data/desafio/enums/TipoFuncionario.java
@@ -1,13 +1,16 @@
 package com.big.data.desafio.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TipoFuncionario {
 	
 	ADMINISTRADOR("Administrador"),
 	COMUM("Comum");
-
 	TipoFuncionario(String string) {
 		
 	}
-	
 
 }
