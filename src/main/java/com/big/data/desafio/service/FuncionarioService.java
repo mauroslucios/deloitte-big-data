352df7b85f872967b20c44a217720320d8e9c2e7
mauroslucios <mauroslucios@gmail.com>
2022-02-10 21:10:00 -0300

    Atualizado FuncionarioService

--- src/main/java/com/big/data/desafio/service/FuncionarioService.java
+++ src/main/java/com/big/data/desafio/service/FuncionarioService.java
@@ -49,49 +49,5 @@ public class FuncionarioService {
 
 	
 
-//	public Funcionario updateFuncionario(Funcionario funcionario,Long id) {
-//		
-//		Funcionario updatedFuncionario = new Funcionario();
-//		Optional<Funcionario> saveFuncionario = funcionarioRepository.findById(id);
-//		Funcionario updateFuncionario = saveFuncionario.get();
-//		
-//		updatedFuncionario = funcionarioRepository.save(updateFuncionario);
-//		return updateFuncionario;
-//	}
-//
-	
-//	public Funcionario findById(@RequestBody Funcionario newfuncionario,@PathVariable Long id) {
-//		return funcionarioRepository.findById(id)
-//				.map(func-> {
-//					func.setNome(newfuncionario.getNome());
-//					func.setCpf(newfuncionario.getCpf());
-//					func.setEmail(newfuncionario.getEmail());
-//					func.setDepartamento(newfuncionario.getDepartamento());
-//					func.setTipo(newfuncionario.getTipo());
-//					func.setSenha(newfuncionario.getSenha());
-//					return funcionarioRepository.save(newfuncionario);
-//				}).orElseGet(()->{
-//					newfuncionario.setId(id);
-//					return funcionarioRepository.save(newfuncionario);
-//				});
-//	}
-//
-//	public Funcionario findById(Long id, Funcionario funcionarioDetalhes) {
-//		return funcionarioRepository.findById(id)
-//				.map(func-> {
-//					func.setNome(funcionarioDetalhes.getNome());
-//					func.setCpf(funcionarioDetalhes.getCpf());
-//					func.setEmail(funcionarioDetalhes.getEmail());
-//					func.setDepartamento(funcionarioDetalhes.getDepartamento());
-//					func.setTipo(funcionarioDetalhes.getTipo());
-//					func.setSenha(funcionarioDetalhes.getSenha());
-//					return funcionarioRepository.save(funcionarioDetalhes);
-//				}).orElseGet(()->{
-//					funcionarioDetalhes.setId(id);
-//					return funcionarioRepository.save(funcionarioDetalhes);
-//				});
-//	}
 
-		
-	
 }
